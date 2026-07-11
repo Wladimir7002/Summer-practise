147bfdb1637a4cac51320de8bb5090cb2a1523a1
Wladimir7002 <vsobolev2020@gmail.com>
2026-07-11 03:10:51 +0300

    Create Contest_5.cpp

---
+++ Contest_5.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ll N;
+    cin >> N;
+
+    ll lcm = 1;
+    for (ll i = 0; i < N; ++i) {
+        ll t;
+        cin >> t;
+        ll g = std::gcd(lcm, t);
+        lcm = lcm / g * t;
+    }
+
+    cout << lcm << endl;
+    return 0;
+}
